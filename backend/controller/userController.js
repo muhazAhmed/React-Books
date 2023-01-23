a1925c4926e82cf16fa25b8e88777dd4113140ac
Muhaz <muhazvla313@gmail.com>
2023-01-23 21:35:29 +0530

    Updated package.json

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,4 +1,4 @@
-const userModel = require("../models/UserModel");
+const userModel = require("../models/userModel");
 const valid = require("../validations/validator.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
